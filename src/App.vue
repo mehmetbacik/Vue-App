a65a5f0091879fa8d1d86f7ec929f2ad2c6b75fe
Mehmet BACIK <79191341+mehmetbacik@users.noreply.github.com>
2023-09-19 20:18:22 +0300

    Update
    Updated for Vue.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content">
+    <form @submit.prevent="addTodo">
+      <div class="field">
+        <label for="" class="label">Todo</label>
+        <div class="control">
+          <input v-model="todo" class="input border" type="text" placeholder="Lorem ipsum dolor sit amet." />
+        </div>
+      </div>
+      <button type="submit" class="button">Add</button>
+    </form>
+    <div v-for="todo in todos" :key="todo.id" class="card my-5 mx-5">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left"></div>
+          <div class="media-content">
+            <p :class="{done: todo.done}" @click="done(todo)" class="title cursor">{{todo.content}}</p>
+            <p class="subtitle">{{todo.done}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {ref} from 'vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const todo = ref('')
+    const todos = ref([])
+
+    function addTodo(){
+      todos.value.push({
+        done: false,
+        content: todo.value,
+        id: Date.now(),
+      })
+      todo.value = ''
+    }
+
+    function done(todo){
+      todo.done = !todo.done
+    }
+    return{
+      todo,
+      todos,
+      addTodo,
+      done,
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
